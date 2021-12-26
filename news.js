141e10764b860a80617d067b87bc258d5e8376fe
NadiaHamed <95628166+NadiaHamed@users.noreply.github.com>
2021-12-26 08:24:54 +0300

    Add files via upload

--- news.js
+++ news.js
@@ -1,23 +1,129 @@
 // api url
 //const api_url ="https://jsonplaceholder.typicode.com/users";
 const key='9832ecfdecb64916a515132f5fb04164';
+    var pageSize=5;
+	var pre=0;  
+	var next=1;  
+	var page=1;
+	var categ='';
+ 
+   var url;
+   var url2   = 'https://newsapi.org/v2/top-headlines?' +
+		 		 'category=general&' +
+		 `pageSize=${pageSize}`+
+		 `&page=${page}`+
+		 `&apiKey=${key}`; 
       
- var api_url = 'https://newsapi.org/v2/everything?' +
-         'q=Apple&' +
-         'from=2021-12-20&' +
-          'sortBy=popularity&' +
-        `apiKey=${key}`;
-        
-        // for search by word 
-// var api_url = 'https://newsapi.org/v2/everything?' +'q=Apple&' +'from=2021-12-20&' +'sortBy=popularity&' +`apiKey=${key}`;
-// Defining async function
-async function getapi(url) {
+async function pagenext(page){
+	next=next+1;
+ 
+	if(categ==''){
+
+		categ='general'
+	  }
+	let keyword = document.getElementById('search').value;
+	if(keyword==''){
+	var url = 'https://newsapi.org/v2/top-headlines?' +
+	'category='+categ+
+	 `&pageSize=${pageSize}`+
+	 `&page=${next}`+
+	 `&apiKey=${key}`;
 	
-	// Storing response
+	}
+	else{	var url = 'https://newsapi.org/v2/top-headlines?' +
+	'category='+categ+
+	 `&pageSize=${pageSize}`+
+	 `&page=${next}`+
+	 `&apiKey=${key}`;}
+   getapi(url,next);
+pre=next-1;
+  // }
+
+
+}
+async function pageprev(page){
+//	pre=next;
+	//pre=pre-1;
+	var url;
+	let keyword = document.getElementById('search').value;
+  if(categ==''){
+
+	categ='general'
+  }
+  
+if(keyword==''){
+	 url = 'https://newsapi.org/v2/top-headlines?' +
+	 'category='+categ+
+	 `&pageSize=${pageSize}`+
+		 `&page=1`+
+		 `&apiKey=${key}`;}
+		 else{
+
+			url = 'https://newsapi.org/v2/top-headlines?' +
+			'category='+categ+
+			 `&pageSize=${pageSize}`+
+			 `&page=${page}`+
+			 `&apiKey=${key}`;
+	 
+
+		 }
+	if (pre<1){
+		if(keyword==''){
+			url = 'https://newsapi.org/v2/top-headlines?' +
+			'category='+categ+
+				`&pageSize=${pageSize}`+
+				`&page=1`+
+				`&apiKey=${key}`;}
+				else{
+	   
+				   url = 'https://newsapi.org/v2/top-headlines?' +
+				   'category='+categ+
+					`&pageSize=${pageSize}`+
+					`&page=${page}`+
+					`&apiKey=${key}`;
+			
+	   
+				}
+	   getapi(url,1);
+	
+	}
+	
+	   else {
+		if(keyword==''){
+			var url = 'https://newsapi.org/v2/top-headlines?' +
+			'category='+categ+
+			 `&pageSize=${pageSize}`+
+			 `&page=${pre}`+
+			 `&apiKey=${key}`;
+		}
+				else{
+	   
+				   
+					var url = 'https://newsapi.org/v2/top-headlines?' +
+					'category='+categ+
+					 `&pageSize=${pageSize}`+
+					 `&page=${pre}`+
+					 `&apiKey=${key}`;
+	   
+				}
+
+		
+		 
+		
+	   getapi(url,pre);
+	   pre=pre-1;
+	   next=pre+1;
+	   }
+
+}
+async function getapi(url,page) {
+
+	 
+ 
 	const response = await fetch(url);
 	
 	// Storing data in form of JSON
-	var data = await response.json();
+	  var data = await response.json();
 	console.log(data.articles);
 	if (response) {
 		hideloader();
@@ -27,7 +133,8 @@ async function getapi(url) {
 
  
 // Calling that async function
-getapi(api_url);
+//categ='';
+getapi(url2,1);
 
 // Function to hide the loader
 function hideloader() {
@@ -36,36 +143,7 @@ function hideloader() {
  
 // Function to define innerHTML for HTML table
 function show(data) {
-	let tab =
-		`<tr>
-		<th>source</th>
-		<th>name</th>
-		<th>author</th>
-		<th>url</th>
-		<th>urlToImage</th>
-		<th>publishedAt</th>
-		<th>content</th>
-		</tr>`;
-	
-	// Loop to access all rows
-	for (let r of data) {
-		tab += `<tr>
-	<td>${r.source} </td>
-	<td>${r.name}</td>
-	<td>${r.author}</td>
-	<td>${r.url}</td>		
-	<td>${r.urlToImage}</td>	
-	<td>${r.publishedAt}</td>	
-	<td>${r.content}</td>	
-
-</tr>`;
-	}
-	// Setting innerHTML as tab variable
-	//document.getElementById("employees").innerHTML = tab;
-
-
-
-
+	 
 
 	document.getElementById('hi').innerHTML = data.map(r => 
 		` <div class="d-flex justify-content-center row">
@@ -75,14 +153,52 @@ function show(data) {
 	   
 		  <div class="col-md-6 mt-1"> <h5>  ${r.source.name}</h5> 
 		  <div class="mt-1 mb-1 spec-1"><span> ${r.content}</span></div> 
-		  </div>
-		  <div class="align-items-center align-content-center col-md-3 border-left mt-1">
 		  <a href=${r.url} target="_blank"> Read more</a>
+		  </div>
+		 
+		  <div class="align-items-center align-content-center col-md-3 border-left mt-1">
+		 <p> Publiched at :${r.publishedAt}</p>
 		   	</div>  </div>
 		</div> <br>`
 	  ).join('')
 	   
 
 
+	
+
+}
+function searchnews(){
+
+	let keyword = document.getElementById('search').value;
+	   const key='9832ecfdecb64916a515132f5fb04164';
+   
+	var api_url  = 'https://newsapi.org/v2/everything?' +
+	'q='+keyword+'&' +
+	'from=2021-12-20&' +
+	 'sortBy=popularity&' +
+	 `pageSize=${pageSize}`+
+	 `&page=${page}`+
+	 `&apiKey=${key}`;
+
+// Calling that async function
+
+categ='';
+getapi(api_url,1);
+
+ 
+
+}
+
+function category(cat) {
+	var category1=cat;
+	categ=category1;
+	const key='9832ecfdecb64916a515132f5fb04164';
+
+var api_url = 'https://newsapi.org/v2/top-headlines?category='+categ+
+`&pageSize=${pageSize}`+
+`&page=${page}`
++`&apiKey=${key}`;
+
+getapi(api_url,1);
 
 }
